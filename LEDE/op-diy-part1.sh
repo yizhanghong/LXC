b22536d2e7a65f8e6b12ebb7ba007eb4da6e2804
Up <8566666@gmail.com>
2024-11-27 13:23:36 +0800

    Create op-diy-part1.sh

---
+++ LEDE/op-diy-part1.sh
@@ -0,0 +1,9 @@
+# 替换packages源为immortalwrt,避免go等依赖包更新不及时
+sed -i '1s|.*|src-git packages https://github.com/immortalwrt/packages.git;openwrt-23.05|' feeds.conf.default
+
+# Add a feed source
+# echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+# echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
+echo "src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main" >> "feeds.conf.default"
+echo "src-git mihomo https://github.com/morytyann/OpenWrt-mihomo.git;main" >> "feeds.conf.default"
