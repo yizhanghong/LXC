2341940656487ed65e12a113e7f986f94c9f5471
Up <8566666@gmail.com>
2024-12-16 21:01:33 +0800

    Update diy-part2.sh

--- LEDE/diy-part2.sh
+++ LEDE/diy-part2.sh
@@ -40,11 +40,11 @@ git clone https://github.com/esirplayground/luci-app-poweroff package/luci-app-p
 #主题
 rm -rf feeds/luci/applications/luci-app-argon-config
 rm -rf feeds/kenzo/luci-app-argon-config
-git clone -b 18.06 https://github.com/jerrykuku/luci-app-argon-config.git feeds/luci/applications/luci-app-argon-config
+git clone https://github.com/jerrykuku/luci-app-argon-config.git feeds/luci/applications/luci-app-argon-config
 
 rm -rf feeds/luci/themes/luci-theme-argon
 rm -rf feeds/kenzo/luci-theme-argon
-git clone -b 18.06 https://github.com/SpeedPartner/luci-theme-argon-18.06-patch.git feeds/luci/themes/luci-theme-argon
+git clone https://github.com/SpeedPartner/luci-theme-argon-18.06-patch.git feeds/luci/themes/luci-theme-argon
 sed -i '41,59d' feeds/luci/themes/luci-theme-argon/luasrc/view/themes/argon/footer.htm
 # git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git feeds/luci/themes/luci-theme-argon
 
